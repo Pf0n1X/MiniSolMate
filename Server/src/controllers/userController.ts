b6cc94b587608533c648281da67f333f4b1e7d0b
adibigler <adibigler96@gmail.com>
2021-01-16 13:57:13 +0200

    :)

--- Server/src/controllers/userController.ts
+++ Server/src/controllers/userController.ts
@@ -3,7 +3,7 @@ import User, { IUser } from "../modules/userModel";
 
 export const addUser = async (req: Request, res: Response) => {
   try {
-    const toAdd = { firstName: "Adi 2" };
+    const toAdd: IUser = { firstName: "Adi 2" };
     const userAdded = await User.create(toAdd);
     res.status(200).json({ message: "User added", ...userAdded });
   } catch (e) {
