40bfbbe742780790cc83d06cbe753ad4e8dab2b5
Tomer <bboypsyfly@gmail.com>
2021-02-11 00:51:34 +0200

    minor animatioain fixes

--- Client/React/mini-solmate/src/routes/Matches.js
+++ Client/React/mini-solmate/src/routes/Matches.js
@@ -6,6 +6,8 @@ const Matches = () => {
     const containerEl = useRef(null);
     const cardEl = useRef(null);
     const userPicEl = useRef(null);
+    const infoEl = useRef(null);
+    const mutualArtistsEl = useRef(null);
 
     return (
         <div className="container"
@@ -27,23 +29,27 @@ const Matches = () => {
                 cardEl.current.style.transform= `rotateY(0deg) rotateX(0deg)`;
                 cardEl.current.style.transition = 'all 0.5s ease';
                 // userPicEl.current.style.transition = 'all 0.5s ease';
-                userPicEl.current.style.transform = 'translateZ(0px)';
+                userPicEl.current.style.transform = 'translateZ(0px) rotateZ(0deg)';
+                infoEl.current.style.transform = 'translateZ(0px)';
+                mutualArtistsEl.current.style.transform = 'translateZ(0px)';
         }}
             onMouseEnter={event => {
                 cardEl.current.style.transition = 'none';
-                userPicEl.current.style.transform = 'translateZ(150px)';
+                userPicEl.current.style.transform = 'translateZ(150px) rotateZ(-10deg)';
+                infoEl.current.style.transform = 'translateZ(175px)';
+                mutualArtistsEl.current.style.transform = 'translateZ(150px)';
             }}>
             <div className="card" ref={cardEl}>
                 <div className="user_pic">
                     <div className="circle" />
                     <img ref={userPicEl} src={profile_pic} />
                 </div>
-                <div className="info">
+                <div ref={infoEl} className="info">
                     <h1 className="name">User Name</h1>
                     <h3 className="age">Age: 21</h3>
                     <h3 className="description">Music Connects People.</h3>
                 </div>
-                <div className="mutual_artists">
+                <div ref={mutualArtistsEl} className="mutual_artists">
                     <img src={profile_pic} alt="" className="artist"/>
                     <img src={profile_pic} alt="" className="artist"/>
                     <img src={profile_pic} alt="" className="artist"/>
