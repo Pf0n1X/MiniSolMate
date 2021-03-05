475b8bd88e0fe88d2211be3d2255b111afacbdb7
edenpd <eden1998p@gmail.com>
2021-03-06 00:23:51 +0200

    broadcast chat

--- Server/src/controllers/chatController.ts
+++ Server/src/controllers/chatController.ts
@@ -2,8 +2,41 @@ import { Request, Response } from "express";
 import { CallbackError } from "mongoose";
 import Chat, { IChat } from "../modules/chatModel";
 import { IUser } from "../modules/userModel";
+import express from "express";
+import * as http from 'http';
+import * as WebSocket from 'ws';
 
-const currentUser = 1;
+const app = express();
+
+const server = http.createServer(express());
+
+const wss = new WebSocket.Server({ server });
+var clients: WebSocket[] = [];
+
+wss.on('connection', (ws: WebSocket) => {
+
+  var id = Math.random();
+  console.log('connection is established : ' + id);
+  clients[id] = ws;
+  clients.push(ws);
+  // Print and echo
+  ws.on('message', (data) => {
+    console.log(`received: ${data}`);
+    for (var i = 0; i < clients.length; i++) {
+      clients[i].send("sent to all");
+    }
+    // ws.send("hi222");
+  })
+});
+
+// setInterval(() => wss.clients.forEach(ws => ws.send(`ping!`)), 2000);
+
+
+// Start server
+server.listen(process.env.PORT || 8999, () => {
+  console.log(`Server started on port ${(<WebSocket.AddressInfo>server.address()).port}`);
+});
+
 
 export const addChat = async (req: Request, res: Response) => {
   try {
@@ -31,10 +64,11 @@ export const updateChat = async (req: Request, res: Response) => {
       UserId1: userBody.UserId1,
       UserId2: userBody.UserId2
     };
-    var query = {'ChatId': userBody.ChatId};
-
-    const chatUpdated = await Chat.findOneAndUpdate(query,toUpdate);
+    var query = { 'ChatId': userBody.ChatId };
+    const chatUpdated = await Chat.findOneAndUpdate(query, toUpdate);
     res.status(200).json({ message: "chat updated", ...chatUpdated });
+
+
   } catch (e) {
     console.log(e);
     res.status(500).send(e);
@@ -42,10 +76,10 @@ export const updateChat = async (req: Request, res: Response) => {
 };
 
 export const getChatsByUser = async (req: Request, res: Response) => {
-  const userBody: IUser = req.body;
+  var userID = Number(req.query.UserId);
 
   await Chat.find(
-    { $or: [ { UserId1: currentUser } , { UserId2: currentUser } ] },
+    { $or: [{ UserId1: userID }, { UserId2: userID }] },
     (err: CallbackError, chats: IChat[]) => {
       if (err) {
         res.status(500).send(err);
