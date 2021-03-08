06659e32b510c61b6a8a8b2e17ad06937ca01f1e
Tomer <bboypsyfly@gmail.com>
2021-03-08 20:24:26 +0200

    Matches selection on server side and presenting a match in client

--- Client/React/mini-solmate/src/routes/Matches.js
+++ Client/React/mini-solmate/src/routes/Matches.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import profile_pic from '../images/profile_pic.jpg';
 import random_chick from '../images/random_chick.jpg';
 import billie_eilish from '../images/billie_eilish.jpg';
@@ -8,16 +8,41 @@ import $ from 'jquery';
 import Popper from 'popper.js';
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 
 const Matches = () => {
+
+    // TODO: Replace with the actual id of the connected user.
+    const CONNECTED_USER_ID = '604639ae4ad4fa1dcc6822e5';
+
+    const [user, setUser] = useState();
+    const [match, setMatch] = useState();
+
+    useEffect(() => {
+
+        // Get the match from the server.
+        axios.get('http://localhost:3001/match?userId=' + CONNECTED_USER_ID)
+        .then((response) => {
+            console.log(response.data)
+            setMatch(response.data)
+
+            // Check which one of the the user fields is the other user
+            // and set the state accordingly.
+            if (response.data.firstUser._id === CONNECTED_USER_ID) {
+                setUser(response.data.secondUser)
+            } else {
+                setUser(response.data.firstUser)
+            }
+        });
+    }, []);
 
     return (
         <div className="wrapper">
             <div className="carousel-container">
                 <div className="user-name">
                     <h2>
-                        Sveta Boronov
+                        {user.firstName} {user.lastName}
                 </h2>
                 </div>
                 <Carousel>
--- Server/src/controllers/matchController.ts
+++ Server/src/controllers/matchController.ts
@@ -1,36 +1,66 @@
 import { Request, Response } from "express";
 import { CallbackError } from "mongoose";
-import Match, { IMatch } from "../modules/matchModel";
+import Match, { IMatch, IMatchModel } from "../modules/matchModel";
 import { IUser } from "../modules/userModel";
+import { Types, ObjectId } from "mongoose";
 
 export const addMatch = async (req: Request, res: Response) => {
-  try {
-    const userBody: IMatch = req.body;
-    const toAdd: IMatch = {
-      UserId1: userBody.UserId1,
-      UserId2: userBody.UserId1,
-      Approve1: userBody.Approve1,
-      Approve2: userBody.Approve2,
-    };
-    const matchAdded = await Match.create(toAdd);
-    res.status(200).json({ message: "Match added", ...matchAdded });
-  } catch (e) {
-    console.log(e);
-    res.status(500).send(e);
-  }
+  // try {
+  //   const userBody: IMatch = req.body;
+  //   const toAdd: IMatch = {
+  //     UserId1: userBody.UserId1,
+  //     UserId2: userBody.UserId1,
+  //     Approve1: userBody.Approve1,
+  //     Approve2: userBody.Approve2,
+  //   };
+  //   const matchAdded = await Match.create(toAdd);
+  //   res.status(200).json({ message: "Match added", ...matchAdded });
+  // } catch (e) {
+  //   console.log(e);
+  //   res.status(500).send(e);
+  // }
 };
 
 export const getMatchesById = async (req: Request, res: Response) => {
-  await Match.find(
-    { UserId1: +(req.query.UserId1 + "") } || {
-      UserId2: +(req.query.UserId2 + ""),
-    },
-    (err: CallbackError, user: IMatch) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.status(200).json(user);
-      }
-    }
-  );
-};
+
+  var userID = req.query.userId?.toString();
+  console.log("The user id is: " + userID);
+  var objid = new Types.ObjectId(userID);
+
+  // Find matches in
+  if (userID !== undefined)
+  await Match.findOne({
+    $or: 
+      [
+        {
+          $and: [
+            {
+              firstUser: userID
+            },
+            {
+              Approve1: false
+            }
+          ]
+        },
+        {
+          $and: [
+            {
+              secondUser: userID
+            },
+            {
+              Approve2: false
+            }
+          ]
+        }
+      ]
+  })
+  .populate("firstUser")
+  .populate("secondUser")
+  .exec((err: CallbackError, user: any) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.status(200).json(user);
+        }
+    });
+}
